2c8bf6ee4e6fef90c6c3a072b8c2537664e68c25
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-01 15:39:17 +0530

    first program

---
+++ Add.py
@@ -0,0 +1,8 @@
+#create two variables (x and y) and assign a value to it
+#create a third variable(sum) and add x and y
+#print the sum
+
+x = 34.4
+y = 265.6
+sum = x + y
+print("The answer is:", sum)
