b290d58f15846e63de38d3990026b9a9f583c0db
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-01 16:24:30 +0530

    third

---
+++ swapFunction.py
@@ -0,0 +1,9 @@
+#take inputs from user for two numbers (x and y)
+
+x=input("Enter the first number: ")
+y=input("Enter the second number: ")
+firstSwapped = int(y)
+secondSwapped = int(x)
+
+print("The 1st swapped number is:", firstSwapped)
+print("The 2nd swapped number is:", secondSwapped)
