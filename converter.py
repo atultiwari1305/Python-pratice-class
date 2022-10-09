ffb5d1d74a3fd8fedd1e43983f27d5db36777748
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-09 13:15:50 +0530

    new

---
+++ converter.py
@@ -0,0 +1,12 @@
+#take user input for x and y
+#if x is less than y, print YAYY
+#else print, NAYY
+
+
+x=float(input("Enter your First number:"))
+y=float(input("Enter your Second number:"))
+
+if x < y:
+    print("YAYY")
+else:
+    print("NAYY")
