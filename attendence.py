d2c06716776715522124b0c9c2052d381c4ea4cf
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-09 13:44:16 +0530

    commit

---
+++ attendence.py
@@ -0,0 +1,5 @@
+attended=float(input("Enter the no. of lectures attended: "))
+total=float(input("Enter total lectures in semester: "))
+
+percentage= (attended/total)*100
+print("Your attendence is:", percentage,"%")
