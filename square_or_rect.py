ca4f8489fba8094fdb10aa96f8ede4c91566a1b9
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-10-09 13:29:53 +0530

    new project

---
+++ square_or_rect.py
@@ -0,0 +1,9 @@
+#take values of length and breadth from user 
+#print if it is a sqaure or not
+
+length=float(input("Enter your length:"))
+breadth=float(input("Enter your breadth:"))
+if length==breadth:
+    print("It is a sqaure")
+else:
+    print("It is not a sqaure")
