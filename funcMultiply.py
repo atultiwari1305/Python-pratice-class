7b97775b19ccb7e24993f17e2ee8a34f98989049
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-11-06 12:51:42 +0530

    functions

---
+++ funcMultiply.py
@@ -0,0 +1,8 @@
+def multiply(num1,num2):
+    product=num1*num2
+    return product
+
+num1=int(input("Enter your 1st number: "))
+num2=int(input("Enter your 2nd number: "))
+x=multiply(2,8)
+print(x)
