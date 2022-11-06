e71d94fa509de587715bd32fb1ac725295fbf19f
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-11-06 13:17:19 +0530

    new

---
+++ ageName.py
@@ -0,0 +1,7 @@
+#create a function and take two agruements of name and age and print it.
+
+def details(name,age):
+    print("Your name is: ",name)
+    print("Your age is: ",age)
+
+details("Atul",19)
