855d7885ebe6f8d38778dfaf7750437d116682a0
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-11-12 16:03:23 +0530

    question

---
+++ collections.py
@@ -0,0 +1,14 @@
+colors=["Green", "Blue", "Red", "Black", "White"]
+cars=["TATA", "Alto", "Nano","Jeep"]
+players=("Dhoni", "Messi", "Dravid")
+
+#colors.insert(2,"Indigo")
+#colors.append("BMW")
+#colors.extend(cars)
+#colors.extend(players)
+#colors.remove("Red")
+#colors.pop(3)
+
+#del colors[2]
+colors.clear()
+print(colors)
