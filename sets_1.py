d6390ed05c4d1dc097eaec63b6d2e8d09e1126a4
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-11-27 16:40:11 +0530

    setDef

---
+++ sets_1.py
@@ -0,0 +1,10 @@
+#set- store multiple values in single variable
+#unordered
+#unchangable/immutable
+#duplicates not allowed
+#non homogeneous
+
+mySet={"car", "boat", "train"}
+if "boat" in mySet:
+    print("boat")
+
