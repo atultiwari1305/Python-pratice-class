a9c52a35f3f6d3c712a60fba82ac54ae5710c274
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-11-27 15:40:22 +0530

    definition

---
+++ listDef.py
@@ -0,0 +1,13 @@
+#list - it stores multiple items in a single variable
+# non homogeneous
+#ordered
+#Changable / mutable
+#allows duplicate values
+
+#append() - add at the end of list
+#insert() - insert item at particular index
+#extend() - add elements from one list to another
+#remove() - remove list element
+#pop() - remove with specific index
+#del and #clear
+#sort() - sort the list in ascending order
