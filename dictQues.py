5411c2c4d58604e4e932f5970abce643b57dd27f
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-12-04 13:42:36 +0530

    question

---
+++ dictQues.py
@@ -0,0 +1,13 @@
+myDictionary={
+    "name":"Atul",
+    "age":19,
+    "roll":37,
+    "class":"Koc28",
+    "percentage":99.9
+}
+
+a=myDictionary.keys()
+print("Keys are:", a)
+
+b=myDictionary.values()
+print("Values are:",b)
