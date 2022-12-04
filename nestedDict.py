a928bb9fc4ee86fe5705e695a7a8a53448260f7b
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-12-04 13:43:39 +0530

    question

---
+++ nestedDict.py
@@ -0,0 +1,13 @@
+myDictionary = {
+    "class":{
+        "student":{
+            "name" : "abc",
+            "marks" : {
+                "python" : 90,
+                "web" : 95
+            }
+        }
+    }
+}
+
+print(myDictionary["class"]["student"]["marks"]["web"])
