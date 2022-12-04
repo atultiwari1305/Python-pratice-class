373ab78b860652107fd0ecf491a113466e2c4b9e
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-12-04 13:41:44 +0530

    learn

---
+++ dictionary_1.py
@@ -0,0 +1,19 @@
+#dictionary are used to store data values in key value pair
+#ordered
+
+myDictionary={
+    "name":"Atul",
+    "age":19,
+    "roll":37,
+    "class":"Koc28",
+    "percentage":99.9
+}
+# print(myDictionary.get("name"))
+# print(myDictionary)
+# print(len(myDictionary))
+
+# a=myDictionary.keys()
+# print(a)
+
+myDictionary.pop("age")
+print(myDictionary)
