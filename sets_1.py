591c7c00f26a6ac617ec1d4a61a37d017c07a9ef
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-12-04 12:30:39 +0530

    learn

--- sets_1.py
+++ sets_1.py
@@ -4,7 +4,15 @@
 #duplicates not allowed
 #non homogeneous
 
-mySet={"car", "boat", "train"}
-if "boat" in mySet:
-    print("boat")
+mySet1={"car", "boat", "train"}
+mySet2={1,2,3,4}
+mySet3={4,5,6,7} 
+# mySet1.update(mySet2)
+# mySet1.discard("car")
+# mySet1.pop()
+#del(mySet1)
+#output=mySet2.union(mySet3)
+#output=mySet2.intersection(mySet3)
+output=mySet2.symmetric_difference(mySet3)
 
+print(output)
