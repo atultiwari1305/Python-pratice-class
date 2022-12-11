684837bede0df7710f4f89cda53b58657727fbe3
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2022-12-11 16:24:18 +0530

    question

---
+++ oops3.py
@@ -0,0 +1,12 @@
+class Car:
+    wheels=4
+
+    def __init__(self,brand,mil):
+        self.brand = brand
+        self.mil = mil
+
+car1 = Car("BMW",10)
+car2 = Car("Tesla",8)
+
+print(car1.brand, car1.mil, car1.wheels)
+print(car2.brand, car2.mil, car2.wheels)
